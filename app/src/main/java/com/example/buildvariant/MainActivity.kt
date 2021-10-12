6ac7224a7c9d524aff8e925eb1cc21a61f73ccbb
Anshul Gour <anshdiv@Anshuls-MacBook-Air.local>
2021-10-13 03:26:39 +0530

    initial commit

---
+++ app/src/main/java/com/example/buildvariant/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.buildvariant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        app_name.text = BuildConfig.appName
+        url.text = BuildConfig.BASE_URL
+    }
+}
